27b89f41f3336277905a558fc7eeac2c508d9ae9
NyanToeSat <k025g1034@g.neec.ac.jp>
2026-07-29 12:06:38 +0900

    commit

--- main.cpp
+++ main.cpp
@@ -1,23 +1,33 @@
-#include <Windows.h>
-#include "KamataEngine.h"
 #include "GameScene.h"
+#include "KamataEngine.h"
+#include <Windows.h>
 
 // Windowsアプリでのエントリーポイント(main関数)
 using namespace KamataEngine;
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	KamataEngine::Initialize(L"GC2B_03_ニャン_トー_セッ_AL2");
-	GameScene* gameScene = new GameScene();
-	gameScene->Initialize();
+	KamataEngine::Initialize(L"GC2B_03_ニャン_トー_セッ_AL3");
+
+	ImGuiManager* imguiManager = ImGuiManager::GetInstance();
+
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+	GameScene* gameScene = new GameScene();
+
+	gameScene->Initialize();
+
 	while (true) {
-		if (!KamataEngine::Update()) {
-			break; 
+		if (KamataEngine::Update()) {
+			break;
 		}
+
+		imguiManager->Begin();
+
 		gameScene->Update();
+
 		// 描画開始
 		dxCommon->PreDraw();
 
 		gameScene->Draw();
+		AxisIndicator::GetInstance()->Draw();
 
 		dxCommon->PostDraw();
 	}
